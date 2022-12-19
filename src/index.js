f7843a57b68b0b6c2700c9ab4b18d7de52655fdf
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-18 20:07:07 -0700

    set up initial grid need to resize. Set up interface div and flexbox layout

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
 const sketch = () => {
   const createTitle = () => {
-    const title = document.body.appendChild(document.createElement("h1"));
+    const titleDiv= document.body.appendChild(document.createElement("div"));
+    const title = titleDiv.appendChild(document.createElement("h1"));
+    titleDiv.setAttribute("class", "title")
     title.textContent = "Technically Sketch";
     //console.log("This function creates the title")
   };
   createTitle();
 
+
+  const createInterface = () => {
+    const interface = document.body.appendChild(document.createElement("div"))
+    interface.setAttribute("class", "interface")
+
   const createControls = () => {
+    const controlDiv = interface.appendChild(document.createElement("div"))
+    controlDiv.setAttribute("class", "control-div")
+    
     const createGridContainer = () => {
-      const gridContainer = document.body.appendChild(
+      const gridContainer = interface.appendChild(
         document.createElement("div")
       );
       gridContainer.setAttribute("class", "grid-container");
 
       //console.log("This function creates the grid container")
       const createGridItems = () => {
+        
         for (let i = 0; i < 16; i++) {
           let gridContainerItem = gridContainer.appendChild(
             document.createElement("div")
           );
           gridContainerItem.setAttribute("class", "grid-container-item");
+          for (let j = 0; j < 16; j++) {
+            let gridContainerItemColumn = gridContainer.appendChild(
+              document.createElement("div")
+            );
+            gridContainerItemColumn.setAttribute(
+              "class",
+              "grid-container-item"
+            );
 
-          let gridContainerSubItem = gridContainerItem.appendChild(
-            document.createElement("div")
-          );
-          gridContainerSubItem.setAttribute("class", "grid-container-sub-item");
-
-          //console.log("this function creates the Divs")
+            //console.log("this function creates the Divs")
+          }
         }
       };
       createGridItems();
     };
     createGridContainer();
   };
-  createControls()
+  createControls();
+}
+createInterface();
 };
 sketch();
