e7a12734d3b7536db99e101755b208d9c7877ae9
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-18 18:39:36 -0700

    setting up grid and containers

--- src/index.js
+++ src/index.js
@@ -1,28 +1,25 @@
 const sketch = () => {
 
-    
+    const createTitle = () => {
+        const title = document.body.appendChild(document.createElement('h1'));
+        title.textContent = "Technically Sketch"
+    }
+    createTitle();
+
+
   const createGridContainer = () => {
-    const gridContainer = document.createElement("div");
-    gridContainer.setAttribute("class", "grid-container");
+        const main = document.getElementById("main")
+        const gridContainer = main.appendChild(document.createElement("div"))
+        gridContainer.setAttribute("class", "grid-container");
 
-    const createGridItems = () => {
-      const gridContainerItem = gridContainer.appendChild(
-        document.createElement("div")
-      );
-      gridContainerItem.setAttribute("class", "grid-container-item");
-      // create 16 div containers
+            const createGridItems = () => {
+                const gridContainerItem = gridContainer.appendChild(document.createElement("div"));
+                gridContainerItem.setAttribute("class", "grid-container-item");
+            } 
+
     };
-  };
-  createGridContainer()
-
-
-
-
-
-
-
-
+    createGridContainer()
 
 
 };
-sketch();
+sketch()
