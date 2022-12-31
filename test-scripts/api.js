795899ff738e68905920071aac396c2a56631aa7
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-30 22:27:44 -0700

    seperating code into two seperate js files

---
+++ test-scripts/api.js
@@ -0,0 +1,51 @@
+function sketch() {
+
+    function titel() {
+        // creates title element
+        // set title text content
+        // set title attributes
+
+    }
+
+    function interface() {
+        // creates interface div
+        // sets div attributes 
+    }
+
+    function grid() {
+        // creates grid div
+        // sets grid attributes
+    }
+
+    function controls(){
+        // creates  control div
+        // creates control div sub div
+        const colors = () => {
+            //creates color control div - child of control div sub div
+            // set's div attributes
+
+            // creates colorChoices div
+
+                // creates color div h3
+                    // sets color div attributes
+                    // sets color div h3 text content
+
+
+                // creates color checkboxes
+                    // set color checkbox attributes
+        }
+
+
+
+        const gridSize = () => {
+            // creates gridZSize control div -- child of control div sub div
+                // create input element
+                // set input attributes
+        }
+        const reset = () => {
+            // creates reset control div -- child of control div sub div
+                // create reset button
+                // set reset button attributes
+        }
+     }
+}
