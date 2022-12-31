49374e702d4719184be6ab412ca04399cda5b068
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-31 01:22:34 -0700

    making DOM additions

--- test-scripts/api.js
+++ test-scripts/api.js
@@ -1,38 +1,76 @@
 function sketch() {
 
-    function titel() {
+    function title() {
         // creates title element
         // set title text content
         // set title attributes
-
+        const title = document.body.appendChild(document.createElement("h1"))
+        title.setAttribute("class", "title")
+        title.setAttribute("id", "title")
+        title.textContent = "Technically Sketch"
     }
 
     function interface() {
         // creates interface div
-        // sets div attributes 
+        // sets div attributes
+        const interface = document.body.appendChild(document.createElement("div"))
+        interface.setAttribute("id", "interface")
+        interface.setAttribute("class", "interface")
     }
 
     function grid() {
         // creates grid div
         // sets grid attributes
+        const grid = interface.appendChild(document.createElement("div"))
+        grid.setAttribute("class", "grid")
+        grid.setAttribute("id", "grid")
     }
 
     function controls(){
         // creates  control div
         // creates control div sub div
+        const controlMenu = interface.appendChild(document.createElement("div"))
+        controlMenu.setAttribute("class", "control-menu")
+        controlMenu.setAttribute("id", "control-menu")
+        const controlMenuDiv = controlMenu.appendChild(document.createElement("div"))
+        controlMenuDiv.setAttribute("class", "control-menu-div")
+        controlMenuDiv.setAttribute("id", "control-menu-div")
+
         const colors = () => {
+            const colorsArray = [
+                "Red",
+                "Orange",
+                "Yellow",
+                "Green",
+                "Blue",
+                "Indigo",
+                "Violet"
+            ]
+
             //creates color control div - child of control div sub div
             // set's div attributes
+            const colorsDiv = controlMenuDiv.appendChild(document.createElement("div"))
+            colorsDiv.setAttribute("class", "color-control")
+            colorsDiv.setAttribute("id", "color-control")
 
             // creates colorChoices div
+            const colorChoices = colorsDiv.appendChild(document.createElement("div"))
+            colorChoices.setAttribute("class", "color-choices")
+            colorChoices.setAttribute("id", "color-choices")
 
                 // creates color div h3
                     // sets color div attributes
                     // sets color div h3 text content
+                    const colorLabel = colorsDiv.appendChild(document.createElement("div"))
+                    colorLabel.textContent = "Colors"
+                    colorLabel.setAttribute("class", "color-label")
+                    colorLabel.setAttribute("id", "color-label")
 
 
                 // creates color checkboxes
                     // set color checkbox attributes
+                    
+
         }
 
 
