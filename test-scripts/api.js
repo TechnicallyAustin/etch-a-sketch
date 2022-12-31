c7693ccc35d148f496e2cef80020607a6d013812
TechnicallyAustin <TechnicallyAustin@gmail.com>
2022-12-31 02:25:17 -0700

    finished base functions will add CSS to test styles

--- test-scripts/api.js
+++ test-scripts/api.js
@@ -8,7 +8,9 @@ function sketchv3() {
         title.setAttribute("class", "title")
         title.setAttribute("id", "title")
         title.textContent = "Technically Sketch"
-    } 
+    };
+
+    // title call
     title()
 
     function interface() {
@@ -26,7 +28,9 @@ function sketchv3() {
         const grid = interface.appendChild(document.createElement("div"));
         grid.setAttribute("class", "grid");
         grid.setAttribute("id", "grid");
-      }
+      };
+
+      // grid call
       grid();
 
       function controls() {
@@ -37,6 +41,11 @@ function sketchv3() {
         );
         controlMenu.setAttribute("class", "control-menu");
         controlMenu.setAttribute("id", "control-menu");
+        
+        const controlLabel = controlMenu.appendChild(document.createElement("h2"))
+        controlLabel.textContent = "Controls"
+        controlLabel.setAttribute("id", "control-label")
+
         const controlMenuDiv = controlMenu.appendChild(
           document.createElement("div")
         );
@@ -73,7 +82,7 @@ function sketchv3() {
           // sets color div attributes
           // sets color div h3 text content
           const colorLabel = colorsDiv.appendChild(
-            document.createElement("div")
+            document.createElement("h3")
           );
           colorLabel.textContent = "Colors";
           colorLabel.setAttribute("class", "color-label");
@@ -89,21 +98,55 @@ function sketchv3() {
             button.setAttribute("class", `${color}`);
           });
         };
+
+        // color call
         colors();
 
         const gridSize = () => {
           // creates gridZSize control div -- child of control div sub div
           // create input element
           // set input attributes
+          const grid = controlMenuDiv.appendChild(document.createElement("div"))
+          const gridLabel = grid.appendChild(document.createElement("h3"))
+          gridLabel.textContent = "Grid Size"
+          grid.setAttribute("id", "grid-control")
+          grid.setAttribute("class", "grid-control")
+
+          const size = grid.appendChild(document.createElement("input"))
+          size.setAttribute("id", "grid-size-input")
+          size.setAttribute("class", "grid-size-input")
         };
+
+        // grid size call
+        gridSize()
+
         const reset = () => {
           // creates reset control div -- child of control div sub div
           // create reset button
           // set reset button attributes
+          const resetDiv = controlMenuDiv.appendChild(document.createElement("div"))
+          const resetLabel = resetDiv.appendChild(document.createElement("h3"))
+          resetLabel.textContent = "Reset"
+          resetDiv.setAttribute("id", "reset-control")
+          resetDiv.setAttribute("class", "reset-control")
+
+          const button = resetDiv.appendChild(document.createElement("button"))
+          button.setAttribute("id", "reset")
+          button.setAttribute("class", "reset")
+          button.setAttribute("value", "reset")
         };
+
+        // reset call
+        reset()
+
+
       }
+      // control call
       controls();
+
     }
+    // interface call
     interface();
-} 
+}
+// sketch call
 sketchv3()
