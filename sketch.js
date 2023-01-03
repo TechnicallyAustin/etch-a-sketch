d0b23c709cbd30f3e4729948594b19e3aad14edd
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-03 14:01:17 -0700

    completed grid interface setup. starting logical operations

--- sketch.js
+++ sketch.js
@@ -1,63 +1,22 @@
-function sketchTwo() {
-    // global variables
-  function title() {}
+function sketchActions() {
+  const gridSelector = document.getElementById("grid")
+  const gridBoxesSelector = document.getElementsByClassName("row")
 
-  function interface() {
-    // interface div 
-    function grid() {
-        // grid div
-      function createGrid() {
-        // grid creation variables
-        function createGridContainer() {
-            // gridContainer variables
-          const items = () => {};
-          const gridEventListeners = () => {};
-        }
+  function gridsize(){
+    const inputSelector = document.getElementById("reset")
+      const changeSize = () => {}
+      const gridEvents = () => {}
+  }
 
-        function deleteGrid() {}
-      }
-    }
+  function gridColor(){
+    const colorsSelector = document.getElementsByClassName("color-boxes")
+      const changeColor = () => {}
+      const colorEvents = () => {}
+  }
 
-    function controls() {
-      // Select the grid and grid columns
-      // create control div - child of interface
-        // create control list - child of control div
-
-      function colors() {
-        // color variables
-        // child of control list
-         const colorArray = [
-           "Red",
-           "Orange",
-           "Yellow",
-           "Green",
-           "Blue",
-           "Indigo",
-           "Violet",
-         ];
-        const colorChoice = () => {
-            // switch case for each color in the Color array
-                // toggles the color evaluated as the class to set the grid divs.
-        };
-        const setGridColor = () => {};
-        // set color toggle
-      }
-
-      function gridSize() {
-        // grid size variables
-        // child of control list
-        const gridChoice = () => {};
-        const newGridSize = () => {
-            // delete grid
-        };
-    }
-
-      function reset() {
-        //reset variables
-        // child of control list
-        const resetChoice = () => {};
-        const resetGrid = () => {};
-      }
-    }
+  function resetGrid(){
+    const resetSelector = document.getElementById("reset")
+      const clearGrid = () => {}
+      const resetEvents = () => {}
   }
 }
