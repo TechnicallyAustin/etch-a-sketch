22a005f1a4cc5d4e164646f1a045bfd1130cc3db
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-03 15:07:22 -0700

    added eventing for each control

--- sketch.js
+++ sketch.js
@@ -2,21 +2,59 @@ function sketchActions() {
   const gridSelector = document.getElementById("grid")
   const gridBoxesSelector = document.getElementsByClassName("row")
 
-  function gridsize(){
-    const inputSelector = document.getElementById("reset")
-      const changeSize = () => {}
-      const gridEvents = () => {}
-  }
+  function gridSize(){
+    const inputSelector = document.getElementById("grid-size-input")
 
+      const changeSize = () => {}
+
+      // adds grid box event listeneres
+      const gridBoxesEvents = () => {
+        for (let i = 0; i < gridBoxesSelector.length; i++){
+          let box = gridBoxesSelector[i]
+          box.addEventListener("click", () =>{
+            console.log("test")
+          })
+        }
+      }; gridBoxesEvents()
+
+      // adds grid size input evenet listeners
+      const gridInputs = () => {
+       inputSelector.addEventListener("click", () => {
+         input = prompt("Enter a size 17 - 100");
+         console.log(input);
+       });
+      }; gridInputs()
+
+
+  } gridSize()
+
+  
   function gridColor(){
     const colorsSelector = document.getElementsByClassName("color-boxes")
       const changeColor = () => {}
-      const colorEvents = () => {}
-  }
 
+      // adds event listeners for each color button, returns the id of the color
+      const colorEvents = () => {
+        for (let i = 0; i < colorsSelector.length; i++){
+          let color = colorsSelector[i]
+          color.addEventListener('click', () => {
+            console.log(`${color.id} clicked`)
+          })
+        }
+      }; colorEvents()
+  }; gridColor()
+
+  // resets the grid back to the starting position
   function resetGrid(){
     const resetSelector = document.getElementById("reset")
       const clearGrid = () => {}
-      const resetEvents = () => {}
-  }
+
+      // adds reset button event listeners
+      const resetEvents = () => {
+        resetSelector.addEventListener('click', () => {
+          console.log("reset clicked")
+        })
+      }; resetEvents()
+  }; resetGrid()
 }
+sketchActions()
