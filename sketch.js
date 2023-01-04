18649c049241c3e09b755d598f0c8d1796d034c7
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-03 23:46:53 -0700

    need to rethink function structure

--- sketch.js
+++ sketch.js
@@ -1,60 +1,73 @@
 function sketchActions() {
-  const gridSelector = document.getElementById("grid")
-  const gridBoxesSelector = document.getElementsByClassName("row")
+  const gridSelector = document.getElementById("grid");
+  const gridBoxesSelector = document.getElementsByClassName("row");
 
-  function gridSize(){
-    const inputSelector = document.getElementById("grid-size-input")
+  function gridSize() {
+    const inputSelector = document.getElementById("grid-size-input");
 
-      const changeSize = () => {}
+    const changeSize = () => {};
 
-      // adds grid box event listeneres
-      const gridBoxesEvents = () => {
-        for (let i = 0; i < gridBoxesSelector.length; i++){
-          let box = gridBoxesSelector[i]
-          box.addEventListener("click", () =>{
-            console.log("test")
-          })
-        }
-      }; gridBoxesEvents()
+    // adds grid box event listeneres
+    const gridBoxesEvents = () => {
+      for (let i = 0; i < gridBoxesSelector.length; i++) {
+        let box = gridBoxesSelector[i];
+        box.addEventListener("click", () => {
+          for (let j = 0; j < gridBoxesSelector.length; j++) {
+            let box = gridBoxesSelector[j];
+            box.addEventListener("mouseover", () => {
+              box.setAttribute("class", "grid-container-item row blue"); // this needs to take the output from color event listeners
+              console.log("hovered");
+            });
+          }
+        });
+      }
+    };
+    gridBoxesEvents();
 
-      // adds grid size input evenet listeners
-      const gridInputs = () => {
-       inputSelector.addEventListener("click", () => {
-         input = prompt("Enter a size 17 - 100");
-         console.log(input);
-       });
-      }; gridInputs()
+    // adds grid size input evenet listeners
+    const gridInputs = () => {
+      inputSelector.addEventListener("click", () => {
+        input = prompt("Enter a size 17 - 100");
+        console.log(input);
+        //return input
+      });
+    };
+    gridInputs();
+  }
+  gridSize();
 
+  function gridColor() {
+    const colorsSelector = document.getElementsByClassName("color-boxes");
+    // adds event listeners for each color button, returns the id of the color
+    const colorEvents = () => {
+      for (let i = 0; i < colorsSelector.length; i++) {
+        let color = colorsSelector[i];
+        color.addEventListener("click", () => {
+          //return color.id
+          console.log(`${color.id}`);
+        });
+      }
+    };
+    colorEvents();
 
-  } gridSize()
-
-  
-  function gridColor(){
-    const colorsSelector = document.getElementsByClassName("color-boxes")
-      const changeColor = () => {}
-
-      // adds event listeners for each color button, returns the id of the color
-      const colorEvents = () => {
-        for (let i = 0; i < colorsSelector.length; i++){
-          let color = colorsSelector[i]
-          color.addEventListener('click', () => {
-            console.log(`${color.id} clicked`)
-          })
-        }
-      }; colorEvents()
-  }; gridColor()
+    const changeColor = (color) => {};
+    changeColor(colorEvents);
+  }
+  gridColor();
 
   // resets the grid back to the starting position
-  function resetGrid(){
-    const resetSelector = document.getElementById("reset")
-      const clearGrid = () => {}
+  function resetGrid() {
+    const resetSelector = document.getElementById("reset");
+    const clearGrid = () => {};
 
-      // adds reset button event listeners
-      const resetEvents = () => {
-        resetSelector.addEventListener('click', () => {
-          console.log("reset clicked")
-        })
-      }; resetEvents()
-  }; resetGrid()
+    // adds reset button event listeners
+    const resetEvents = () => {
+      resetSelector.addEventListener("click", () => {
+        console.log("reset clicked");
+      });
+    };
+    resetEvents();
+  }
+  resetGrid();
 }
-sketchActions()
+sketchActions();
--- test-scripts/api.js
+++ test-scripts/api.js
@@ -1,205 +1,182 @@
 function sketchv3() {
+  function title() {
+    // creates title element
+    // set title text content
+    // set title attributes
+    const title = document.body.appendChild(document.createElement("h1"));
+    title.setAttribute("class", "title");
+    title.setAttribute("id", "title");
+    title.textContent = "Technically Sketch";
+  }
 
-    function title() {
-        // creates title element
-        // set title text content
-        // set title attributes
-        const title = document.body.appendChild(document.createElement("h1"))
-        title.setAttribute("class", "title")
-        title.setAttribute("id", "title")
-        title.textContent = "Technically Sketch"
-    };
+  // title call
+  title();
 
-    // title call
-    title()
+  function interface() {
+    // creates interface div
+    // sets div attributes
+    const interface = document.body.appendChild(document.createElement("div"));
+    interface.setAttribute("id", "interface");
+    interface.setAttribute("class", "interface");
 
-    function interface() {
-      // creates interface div
-      // sets div attributes
-      const interface = document.body.appendChild(
+    function grid() {
+      // creates grid div
+      // sets grid attributes
+      const gridContainer = interface.appendChild(
         document.createElement("div")
       );
-      interface.setAttribute("id", "interface");
-      interface.setAttribute("class", "interface");
+      gridContainer.setAttribute("class", "grid-container");
 
-      function grid() {
-        // creates grid div
-        // sets grid attributes
-        const gridContainer = interface.appendChild(document.createElement("div"))
-        gridContainer.setAttribute("class", "grid-container")
+      const grid = gridContainer.appendChild(document.createElement("div"));
+      grid.setAttribute("class", "grid");
+      grid.setAttribute("id", "grid");
 
-        const grid = gridContainer.appendChild(document.createElement("div"));
-        grid.setAttribute("class", "grid");
-        grid.setAttribute("id", "grid");
+      function createGrid(n = 16) {
+        for (let i = 0; i < n; i++) {
+          let gridDiv = grid.appendChild(document.createElement("div"));
+          gridDiv.setAttribute("class", "grid-container-item col");
+          for (let j = 0; j < n; j++) {
+            let gridContainerItemColumn = gridDiv.appendChild(
+              document.createElement("div")
+            );
+            gridContainerItemColumn.setAttribute(
+              "class",
+              "grid-container-item row"
+            );
 
-        function createGrid(n = 16) {
-            //const gridDiv = document.getElementById("grid")
-            //for (let i = 0; i < 16; i++){
-            //    let gridItem = gridDiv.appendChild(document.createElement("div"))
-            //    gridItem.setAttribute("class", "col")
-//
-            //    for (let j = 0; j < 16; j++){
-            //    let col = gridItem.appendChild(document.createElement("div"));
-            //    col.setAttribute("class", "row");
-            //    }
-            //}
-
-
-             for (let i = 0; i < n; i++) {
-               let gridDiv = grid.appendChild(
-                 document.createElement("div")
-               );
-               gridDiv.setAttribute(
-                 "class",
-                 "grid-container-item col"
-               );
-               for (let j = 0; j < n; j++) {
-                 let gridContainerItemColumn = gridDiv.appendChild(
-                   document.createElement("div")
-                 );
-                 gridContainerItemColumn.setAttribute(
-                   "class",
-                   "grid-container-item row"
-                 );
-
-                 //console.log("this function creates the Divs")
-               }
-             }
-
-            
+            //console.log("this function creates the Divs")
+          }
         }
+      }
 
-        //creaete grid call
-        createGrid()
+      //creaete grid call
+      createGrid();
+    }
 
+    // grid call
+    grid();
 
+    function controls() {
+      // creates  control div
+      // creates control div sub div
+      const controlContainer = interface.appendChild(
+        document.createElement("div")
+      );
+      controlContainer.setAttribute("class", "control-container");
 
+      const controlMenu = controlContainer.appendChild(
+        document.createElement("div")
+      );
+      controlMenu.setAttribute("class", "controls");
+      controlMenu.setAttribute("id", "controls");
 
+      const controlLabel = controlMenu.appendChild(
+        document.createElement("h2")
+      );
+      controlLabel.textContent = "Controls";
+      controlLabel.setAttribute("id", "control-label");
+
+      const controlMenuDiv = controlMenu.appendChild(
+        document.createElement("div")
+      );
+      controlMenuDiv.setAttribute("class", "control-menu-div");
+      controlMenuDiv.setAttribute("id", "control-menu-div");
+
+      const colors = () => {
+        const colorsArray = [
+          "red",
+          "orange",
+          "yellow",
+          "green",
+          "blue",
+          "indigo",
+          "violet",
+          "black",
+        ];
+
+        //creates color control div - child of control div sub div
+        // set's div attributes
+        const colorsDiv = controlMenuDiv.appendChild(
+          document.createElement("div")
+        );
+        colorsDiv.setAttribute("class", "color-control");
+        colorsDiv.setAttribute("id", "color-control");
+
+        // creates color div h3
+        // sets color div attributes
+        // sets color div h3 text content
+        const colorLabel = colorsDiv.appendChild(document.createElement("h3"));
+        colorLabel.textContent = "Colors";
+        colorLabel.setAttribute("class", "color-label");
+        colorLabel.setAttribute("id", "color-label");
+
+        // creates colorChoices div
+        const colorChoices = colorsDiv.appendChild(
+          document.createElement("div")
+        );
+        colorChoices.setAttribute("class", "color-choices");
+        colorChoices.setAttribute("id", "color-choices");
+
+        // creates color checkboxes
+        // set color checkbox attributes
+        colorsArray.forEach((color) => {
+          let button = colorChoices.appendChild(
+            document.createElement("checkbox")
+          );
+          button.setAttribute("id", `${color}`);
+          button.setAttribute("class", `${color} color-boxes`);
+        });
       };
 
-      // grid call
-      grid();
+      // color call
+      colors();
 
-      function controls() {
-        // creates  control div
-        // creates control div sub div
-        const controlContainer = interface.appendChild(document.createElement("div"))
-        controlContainer.setAttribute("class", "control-container")
+      const gridSize = () => {
+        // creates gridZSize control div -- child of control div sub div
+        // create input element
+        // set input attributes
+        const grid = controlMenuDiv.appendChild(document.createElement("div"));
+        const gridLabel = grid.appendChild(document.createElement("h3"));
+        gridLabel.textContent = "Grid Size";
+        grid.setAttribute("id", "grid-control");
+        grid.setAttribute("class", "grid-control");
 
+        const size = grid.appendChild(document.createElement("input"));
+        size.setAttribute("id", "grid-size-input");
+        size.setAttribute("class", "grid-size-input");
+        size.setAttribute("placeholder", "Enter a size 17-100");
+      };
 
-        const controlMenu = controlContainer.appendChild(
+      // grid size call
+      gridSize();
+
+      const reset = () => {
+        // creates reset control div -- child of control div sub div
+        // create reset button
+        // set reset button attributes
+        const resetDiv = controlMenuDiv.appendChild(
           document.createElement("div")
         );
-        controlMenu.setAttribute("class", "controls");
-        controlMenu.setAttribute("id", "controls");
-        
-        const controlLabel = controlMenu.appendChild(document.createElement("h2"))
-        controlLabel.textContent = "Controls"
-        controlLabel.setAttribute("id", "control-label")
+        const resetLabel = resetDiv.appendChild(document.createElement("h3"));
+        resetLabel.textContent = "Reset";
+        resetDiv.setAttribute("id", "reset-control");
+        resetDiv.setAttribute("class", "reset-control");
 
-        const controlMenuDiv = controlMenu.appendChild(
-          document.createElement("div")
-        );
-        controlMenuDiv.setAttribute("class", "control-menu-div");
-        controlMenuDiv.setAttribute("id", "control-menu-div");
+        const button = resetDiv.appendChild(document.createElement("button"));
+        button.setAttribute("id", "reset");
+        button.setAttribute("class", "reset");
+        button.setAttribute("value", "reset");
+        button.textContent = "Reset Grid";
+      };
 
-        const colors = () => {
-          const colorsArray = [
-            "red",
-            "orange",
-            "yellow",
-            "green",
-            "blue",
-            "indigo",
-            "violet",
-          ];
-
-          //creates color control div - child of control div sub div
-          // set's div attributes
-          const colorsDiv = controlMenuDiv.appendChild(
-            document.createElement("div")
-          );
-          colorsDiv.setAttribute("class", "color-control");
-          colorsDiv.setAttribute("id", "color-control");
-
-          // creates color div h3
-          // sets color div attributes
-          // sets color div h3 text content
-          const colorLabel = colorsDiv.appendChild(
-            document.createElement("h3")
-          );
-          colorLabel.textContent = "Colors";
-          colorLabel.setAttribute("class", "color-label");
-          colorLabel.setAttribute("id", "color-label");
-
-          // creates colorChoices div
-          const colorChoices = colorsDiv.appendChild(
-            document.createElement("div")
-          );
-          colorChoices.setAttribute("class", "color-choices");
-          colorChoices.setAttribute("id", "color-choices");
-
-          // creates color checkboxes
-          // set color checkbox attributes
-          colorsArray.forEach((color) => {
-            let button = colorChoices.appendChild(
-              document.createElement("checkbox")
-            );
-            button.setAttribute("id", `${color}`);
-            button.setAttribute("class", `${color} color-boxes`);
-          });
-        };
-
-        // color call
-        colors();
-
-        const gridSize = () => {
-          // creates gridZSize control div -- child of control div sub div
-          // create input element
-          // set input attributes
-          const grid = controlMenuDiv.appendChild(document.createElement("div"))
-          const gridLabel = grid.appendChild(document.createElement("h3"))
-          gridLabel.textContent = "Grid Size"
-          grid.setAttribute("id", "grid-control")
-          grid.setAttribute("class", "grid-control")
-
-          const size = grid.appendChild(document.createElement("input"))
-          size.setAttribute("id", "grid-size-input")
-          size.setAttribute("class", "grid-size-input")
-          size.setAttribute("placeholder", "Enter a size 17-100")
-        };
-
-        // grid size call
-        gridSize()
-
-        const reset = () => {
-          // creates reset control div -- child of control div sub div
-          // create reset button
-          // set reset button attributes
-          const resetDiv = controlMenuDiv.appendChild(document.createElement("div"))
-          const resetLabel = resetDiv.appendChild(document.createElement("h3"))
-          resetLabel.textContent = "Reset"
-          resetDiv.setAttribute("id", "reset-control")
-          resetDiv.setAttribute("class", "reset-control")
-
-          const button = resetDiv.appendChild(document.createElement("button"))
-          button.setAttribute("id", "reset")
-          button.setAttribute("class", "reset")
-          button.setAttribute("value", "reset")
-          button.textContent = "Reset Grid"
-        };
-
-        // reset call
-        reset()
-
-
-      }
-      // control call
-      controls();
-
+      // reset call
+      reset();
     }
-    // interface call
-    interface();
+    // control call
+    controls();
+  }
+  // interface call
+  interface();
 }
 // sketch call
-sketchv3()
+sketchv3();
